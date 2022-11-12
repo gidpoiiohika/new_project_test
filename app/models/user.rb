a7ad09f4d0bd1158164decc09b5cd51fc896874b
Yaroslav <adacer73044210@gmail.com>
2022-11-12 20:12:09 +0200

    add jwt devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :validatable, :confirmable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :validatable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   enum role:   %i[admin author],        _default: "author"
   enum status: %i[blocked not_blocked], _default: "not_blocked"
+
+  def jwt_payload
+    super
+  end
 end
