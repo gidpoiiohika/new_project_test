d2fc498698e6c6f50bdc6b8d051f5865ae2d73b7
Yaroslav <adacer73044210@gmail.com>
2022-11-13 22:08:20 +0200

    add soft delete to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,11 +10,26 @@ class Article < ApplicationRecord
 
   enum status: %i[published hidden], _default: "published"
 
+  scope :not_deleted, lambda { where(deleted_at: nil) }
+  scope :deleted, lambda { where("#{self.table_name}.deleted_at IS NOT NULL") }
+
   STATUS = %w{ published hidden }
 
   STATUS.each do |status_article|
     define_method "#{status_article}?" do 
       status == status_article  
     end
+  end
+
+  def destroy
+    self.update(deleted_at: DateTime.current)
+  end
+
+  def delete
+    destroy
+  end
+
+  def deleted?
+    self.deleted_at.present?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_150141) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_200242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_150141) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.datetime "deleted_at"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["category_id"], name: "index_articles_on_category_id", unique: true
     t.index ["title"], name: "index_articles_on_title"
